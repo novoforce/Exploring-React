4aa37ec4906da16bb7722e6fb9691fe2e08dbaec
novoforce <ashish.space5@gmail.com>
2021-11-10 18:59:40 +0530

    textutils files added

---
+++ textutils/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+      <Navbar title={"TextUtils"} about="About"></ Navbar>
+      <div className="container my-3">
+        <TextForm heading="Enter your text to analyze"/>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ textutils/src/components/TextForm.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleUpClick= ()=>{
+        let newText= text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange= (event)=>{
+        setText(event.target.value);
+    }
+
+    const handleDownClick= ()=>{
+        let newText= text.toLowerCase();
+        setText(newText);
+    }
+    const [text,setText] = useState('Enter text here');
+
+    return (
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange= {handleOnChange} id="myBox" rows="8"></textarea>
+                <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
+            </div>
+        </div>
+        <div className="container my-2">
+            <h1>Your text Summary</h1>
+            <p>{text.split(" ").length} words are present and {text.length} characters are present</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+
+        </>
+    )
+}
